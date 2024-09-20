42d0bc3307bc7b490456ad332eb6aec562175e31
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2024-09-20 08:49:04 -0400

    Add Bulma css to style buttons, select and score display.

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const p1Btn = document.querySelector('#p1-button')
 const p2Btn = document.querySelector('#p2-button')
 const resetBtn = document.querySelector('#reset-button')
 
+const playTo = document.querySelector('#play-to')
+
 let p1Score = 0;
 let p2Score = 0;
-let winningScore = 5;
+let winningScore = 3;
 let isGameOver = false;
 
 p1Btn.addEventListener('click', () => {
@@ -24,8 +26,14 @@ resetBtn.addEventListener('click', () => {
     isGameOver = false;
     p1Score = 0;
     p2Score = 0;
+    p1Counter.classList.remove('winner', 'loser')
+    p2Counter.classList.remove('winner', 'loser')
     updateDisplay(p1Score, p1Counter)
     updateDisplay(p2Score, p2Counter)
+})
+
+playTo.addEventListener('change', function() {
+    winningScore = parseInt(this.value)
 })
 
 function updateScore(playerScore, playerCounter) {
@@ -34,6 +42,13 @@ function updateScore(playerScore, playerCounter) {
         updateDisplay(playerScore, playerCounter)
         if(playerScore === winningScore) {
             isGameOver = true;
+            playerCounter.classList.add('winner')
+            const scoreContainer = document.querySelector('#score-container');
+            scoreContainer.querySelectorAll('span').forEach(e => {
+                if(e !== playerCounter) {
+                    e.classList.add('loser')
+                }
+            })
         }
     }
 }
